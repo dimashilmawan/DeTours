d4884824df337e4dc15348cb9e099ea346884f8b
Dimas hilmawan <hilmawan22@gmail.com>
2020-11-27 00:14:48 +0700

    edit bookingControllers edit tour models

--- controllers/authController.js
+++ controllers/authController.js
@@ -91,6 +91,7 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
     user.passwordResetToken = undefined;
     user.passwordResetExpires = undefined;
     await user.save({ validateBeforeSave: false });
+    console.log(err);
     return next(
       new AppError(
         'There was an error sending the email, please try again later',
