7bd5bc0af8eac5fc3a7d6f7d63dea194c9d1a092
Dimas hilmawan <hilmawan22@gmail.com>
2020-11-26 23:28:04 +0700

    edit bookingControllers imagessss

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -19,9 +19,9 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     line_items: [
       {
         name: `${tour.name} Tour`,
-        images: `${req.protocol}://${req.get('host')}/img/tours/${
-          tour.imageCover
-        }`,
+        images: [
+          `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`
+        ],
         amount: tour.price * 100,
         currency: 'idr',
         quantity: 1
